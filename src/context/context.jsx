498d2c0e035a0d1f0768ab48e0b42ead07b49599
Ankit Sharma <asharma@dainsta.com>
2024-01-20 22:01:05 +0530

    remove logs

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -4,14 +4,13 @@ import {createContext, useState} from "react";
 export const ApplicationContext = createContext({
     currentUser:null,
     setCurrentUser: ()=>null,
-    todos:[],
-    setTodos:()=>null,
+
 });
 
 export const ApplicationContextProvider = ({children}) =>{
     const [currentUser,setCurrentUser] = useState(null);
-    const [todos,setTodos] = useState([]);
 
-    const value = {currentUser,setCurrentUser,todos,setTodos}
+
+    const value = {currentUser,setCurrentUser}
     return <ApplicationContext.Provider value={value}>{children}</ApplicationContext.Provider>
 }
